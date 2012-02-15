a3be0b91a3ec9f93b1142b70027f862a1d4bfe81
Dominik Guzei <dominik_guzei@gmx.at>
2012-02-15 11:52:21 +0100

    Initial commit with rails 3.2 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urgify::Application.config.secret_token = '1e29a6a1b1fd92d28e5252a8cc3e73bc0406445c93c42e19ec68a6885630220e7c99c176750dbd78cfed0a9e950494e827706d496b2dfab7e802e8a1cef452ae'
